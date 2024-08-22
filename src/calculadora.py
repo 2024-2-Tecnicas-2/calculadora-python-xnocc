92120c264f2615e1bfe36462201105be6e905ce0
none <none@gmail.com>
2024-08-21 19:00:45 -0500

    Solución

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,6 +13,12 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
+        case '^':
+            # Operación potencia
+            respuesta = numero1 ** numero2
+        case '%':
+            # Operación residuo
+            respuesta = numero1 % numero2
 
         # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
 
